694314e15d92287cd65febe95938729d4afe4bed
Cristhian Ocola <ocolapintoc@gmail.com>
2024-05-04 14:31:45 -0500

    Fix/dto_swagger

--- src/auth/dto/create-user-dto.ts
+++ src/auth/dto/create-user-dto.ts
@@ -1,9 +1,15 @@
-import { Exclude } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'El correo electrónico del usuario' })
   email: string;
+
+  @ApiProperty({ description: 'El nombre del usuario' })
   name: string;
+
+  @ApiProperty({ description: 'El apellido del usuario' })
   lastName: string;
+
+  @ApiProperty({ description: 'La contraseña del usuario' })
   password: string;
-  
 }
